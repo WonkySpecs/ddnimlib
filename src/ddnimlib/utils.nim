2987de672d6b2c454e23cdb1d4c65c458c961e31
Will Taylor <w1ll100@hotmail.co.uk>
2021-04-05 23:26:08 +0100

    Rect -> vec helpers

--- src/ddnimlib/utils.nim
+++ src/ddnimlib/utils.nim
@@ -6,3 +6,6 @@ template r*(x, y, w, h: int): Rect =
 
 template r*(pos: Vec[2], size: Vec[2]): Rect =
   rect(pos.x.cint, pos.y.cint, size.x.cint, size.y.cint)
+
+func pos*(r: Rect): Vec[2] = vec(r[0].int, r[1].int)
+func size*(r: Rect): Vec[2] = vec(r[2].int, r[3].int)
