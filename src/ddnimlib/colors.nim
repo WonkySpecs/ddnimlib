5e7d4de423167401727ce174befc05c37f74ea02
Will Taylor <w1ll100@hotmail.co.uk>
2021-04-19 18:28:58 +0100

    Common colors
    Sacrificing us for consistency with sdl

---
+++ src/ddnimlib/colors.nim
@@ -0,0 +1,8 @@
+import sdl2
+
+const
+  black* = Color((0'u8, 0'u8, 0'u8, 255'u8))
+  red* = Color((255'u8, 0'u8, 0'u8, 255'u8))
+  green* = Color((0'u8, 255'u8, 0'u8, 255'u8))
+  blue* = Color((0'u8, 0'u8, 255'u8, 255'u8))
+  white* = Color((255'u8, 255'u8, 255'u8, 255'u8))
